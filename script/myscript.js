5d023470a1f3a4e11b970aa563884757f97f8650
Svitlana1996 <lanaovsiichuk@gmail.com>
2019-07-28 21:47:49 +0300

    first commit

---
+++ script/myscript.js
@@ -0,0 +1,226 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '.arrow--previous',
+        nextArrow: '.arrow--next'    
+                                 
+    })
+
+});
+$(document).ready(function(){
+    $('.slider-second').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.arrow--previous',
+        nextArrow: '.arrow--next',
+        arrows:true
+                                 
+    })
+   });
+
+ function initMap() {
+    var uluru = {lat:-7.928034, lng:112.644363}; 
+    
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 16, 
+            center: uluru,
+            styles: [
+                {
+                  "featureType": "administrative.land_parcel",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "administrative.land_parcel",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "landscape.man_made",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "landscape.man_made",
+                  "elementType": "labels.icon",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "landscape.natural.landcover",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "poi",
+                  "elementType": "labels.text",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "poi.attraction",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "poi.business",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "road",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "road",
+                  "elementType": "labels.icon",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "road.arterial",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "road.local",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "road.local",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "transit",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "transit",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "transit.line",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "transit.line",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "water",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "water",
+                  "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                }
+              ],
+            disableDefaultUI: true 
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            icon:'../img/marker.png'
+          });
+ };
+ $("a").click(function () {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    $('html, body').animate({ scrollTop: destination }, 1600);
+    return false;
+});
+
+$('.bar__contact').click(function(){
+    if($(this).next('.bar__info')) {
+        $(this).next('.bar__info').toggleClass('bar__info-active')
+    }else {
+        $('.bar__info').removeClass('bar__info-active');
+        $(this).next('.bar__info').addClass('bar__info-active')
+    }
+});
+
+ 
+    
---
+++ script/vuescript.js
@@ -0,0 +1,79 @@
+
+ Vue.component('user-info', {
+     props:{
+         workers: {
+             type: Array
+         }
+     },
+     template:`
+     <div class="slider-second__item">
+         <img class="slider-second__image" v-bind:src="workers.image" alt="">
+                         
+         <div class="slider-second__bgrd">
+             <img v-bind:src="background" alt="">
+         </div>
+         <div class="slider-second__wrap">
+             <h3 class="slider-second__heading">{{workers.name}}</h3>
+             <p class="slider-second__text">{{workers.position}}</p>
+         </div>
+                             
+         <div class="slider-second__footer">
+             <img v-for="social in workers.workerSocial" v-bind:src="social" alt="">
+             
+         </div>
+     </div>
+     `,
+     data: function(){
+         return {
+             background: "img/Grid-Nama-Posisi.png",
+            //  workers: [
+            //     {
+            //         workerId:1,
+            //         image: 'img/boy.png',
+            //         name: 'Miguel Obrien',
+            //         position: 'CEO & Founder',
+            //         workerSocial: ['img/linked-in.png', 'img/google+.png', 'img/facebook.png', 'img/twitter.png']
+            //     },
+            //     {
+            //        workerId:2,
+            //        image: 'img/red-girl.png',
+            //        name: 'Jackie Carroll',
+            //        position: 'Art Director',
+            //        workerSocial: ['img/behance.png','img/linked-in.png', 'img/google+.png', 'img/facebook.png', 'img/twitter.png']
+                            
+            //     },
+            // ]
+             
+         }
+          
+     }
+ }
+  
+ );
+
+ var vm = new Vue ({
+     el:'#slider2',
+     data: function() {
+         return {
+            workers: [
+                {
+                    workerId:1,
+                    image: 'img/boy.png',
+                    name: 'Miguel Obrien',
+                    position: 'CEO & Founder',
+                    workerSocial: ['img/linked-in.png', 'img/google+.png', 'img/facebook.png', 'img/twitter.png']
+                },
+                {
+                   workerId:2,
+                   image: 'img/red-girl.png',
+                   name: 'Jackie Carroll',
+                   position: 'Art Director',
+                   workerSocial: ['img/behance.png','img/linked-in.png', 'img/google+.png', 'img/facebook.png', 'img/twitter.png']
+                            
+                },
+            ]
+         }
+        
+
+     }
+ })
