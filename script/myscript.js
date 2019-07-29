5ce2395189c098666e332c345d3482af7c2b9849
Svitlana1996 <lanaovsiichuk@gmail.com>
2019-07-29 21:14:51 +0300

    first commit

--- script/myscript.js
+++ script/myscript.js
@@ -24,6 +24,13 @@ $(document).ready(function(){
         arrows:true,
         responsive: [
             {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+
+            },
+            {
                 breakpoint: 960,
                 settings: {
                     slidesToShow: 2
